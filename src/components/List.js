816d6973126b953d3c848b867ad6fc68e113f79c
Alexander <allgud83@gmail.com>
2021-04-01 15:53:24 +0300

    Поменял кнопки удаления. Выровнял контент в списке

--- src/components/List.js
+++ src/components/List.js
@@ -3,15 +3,26 @@ import { FirebaseContext } from '../context/firebase/firebaseContext'
 
 const List = () => {
 
-    const {list} = useContext(FirebaseContext)
+    const {list, removeItem} = useContext(FirebaseContext)
 
 
     return (
         <ul className="list-group">
             {list.map(item => (
                     <li key={item.id} className="list-group-item list_item" >
-                        <strong className="item_title"><span className="item_title_num">{item.id}</span>{item.title}</strong>
+                        <span className="item_title_num">{item.id}</span><strong className="item_title">{item.title}</strong>
                         <span className="item_date">{item.date}</span>
+                        <button 
+                                type="button" 
+                                className="btn btn-outline-danger btn-sm"
+                                onClick={() => removeItem(item.id)}
+                        >           <span>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+                                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                                    </svg>
+                                    </span> 
+                                </button>
                     </li> 
             ))}
         </ul>
